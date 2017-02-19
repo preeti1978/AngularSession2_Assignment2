8162c4f7d986b5dd3b7f30b6ced6d827b8cbdde0
preeti1978 <lekshmy.money@gmail.com>
2017-02-19 14:24:02 +0530

    Assignment of Session 2 of Angular Js course
    Base and Derived Class and Modules assignment. Qno 2 of Assignment

---
+++ Part1/calculation.ts
@@ -0,0 +1,17 @@
+<!--calculation.js-->
+
+export function square(x) {
+    return x * x;
+}
+export function add(x, y) {
+    return x+y;
+}
+
+export function subtract(x, y) {
+    return x-y;
+}
+
+
+export function multiply(x, y) {
+    return x*y;
+}
---
+++ Part1/main.ts
@@ -0,0 +1,7 @@
+
+<!--main.ts-->
+import {square,add,subtract,multiply} as from 'calculation';//importing all these functions from calculation.js
+console.log(square(11)); // 121
+console.log(add(4, 3)); // 7
+console.log(subtract(5,2));//3
+console.log(multiply(3,2));
